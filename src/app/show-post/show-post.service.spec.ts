7b3756aec9ab9a63ac7d186305fc1583d4e2be03
Dehow01 <dehow@mail.ru>
2020-05-14 16:46:31 +0300

    add tests for service

--- src/app/show-post/show-post.service.spec.ts
+++ src/app/show-post/show-post.service.spec.ts
@@ -1,15 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-import { LoginService } from './login.service';
+import { ShowPostService } from './show-post.service';
 import {HttpClientModule} from '@angular/common/http';
 
 describe('LoginService', () => {
   beforeEach(() => TestBed.configureTestingModule({ imports: [
       HttpClientModule
     ],
-    providers: [LoginService]
+    providers: [ShowPostService]
   }));
   it('should be created', () => {
-    const service: LoginService = TestBed.get(LoginService);
+    const service: ShowPostService = TestBed.get(ShowPostService);
     expect(service).toBeTruthy();
   });
 });
